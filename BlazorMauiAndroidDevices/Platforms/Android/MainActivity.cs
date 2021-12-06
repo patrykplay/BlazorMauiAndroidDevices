64e27e655d97398f2f7d081c0544152c44abaede
Patryk Parzoch <patryk.parzoch@gmail.com>
2021-12-06 22:49:34 +0100

    Test commit

---
+++ BlazorMauiAndroidDevices/Platforms/Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.Maui;
+
+namespace BlazorMauiAndroidDevices
+{
+	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            RequestPermissions(new[] { Manifest.Permission.Camera }, 0);
+        }
+    }
+}
---
+++ BlazorMauiAndroidDevices/wwwroot/device.js
@@ -0,0 +1,53 @@
+﻿
+function requestMedia() {
+
+    // Create request options
+    let options = {
+        audio: true,
+        video: true
+    };
+
+
+    // Warning: Below commented code causes "Illegal invocation error"
+    //// Set up proper function
+    //navigator.mediaDevices.getUserMedia =
+    //    navigator.getUserMedia ||
+    //    navigator.webkitGetUserMedia ||
+    //    navigator.mozGetUserMedia ||
+    //    navigator.msGetUserMedia;
+
+    try {
+        // Request user media
+        navigator.mediaDevices
+            .getUserMedia(options)
+            .then(gotLocalStream)
+            .catch(logError);
+    }
+    catch (e) {
+        logError(e);
+    }
+
+}
+
+// Receives local stream data 
+function gotLocalStream(stream) {
+
+    // Get 'video' html control
+    var videoLocalPanel = document.getElementById("LOCAL_VIDEO_ID");
+
+    if ("srcObject" in videoLocalPanel) {
+        videoLocalPanel.srcObject = stream;
+    } else {
+        videoLocalPanel.src = window.URL.createObjectURL(stream);
+    }
+
+}
+
+function logError(e) {
+
+    // Get 'label' html control
+    var label = document.getElementById("LABEL_ID");
+
+    label.value = e;
+
+}
